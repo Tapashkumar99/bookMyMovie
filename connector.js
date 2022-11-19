2097a0a95972af79505c197000784190ff4c343c
tapash kumar <tapashkumar118@gmail.com>
2022-11-19 15:46:06 +0530

    deploy

---
+++ connector.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const mongodb = require('mongodb');
+
+const mongoURI = process.env.mongoURI
+
+let mongoose = require('mongoose');
+//  Importing Mongoose Schema from schema.js
+const { bookMovieSchema } = require('./models/schema')
+
+//  Establishing connection between MongoDB and express.js server
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { console.log("connection established with mongodb server online"); })
+    .catch(err => {
+        console.log("error while connection", err)
+    });
+let collection_connection = mongoose.model('bookmovietickets', bookMovieSchema)
+
+
+exports.connection = collection_connection;
