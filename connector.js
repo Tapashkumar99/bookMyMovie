bfdf5fbd6447706e0f0cf01ff006c4677c21bc14
tapash kumar <tapashkumar118@gmail.com>
2022-11-19 16:08:34 +0530

    mongo

--- connector.js
+++ connector.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongodb = require('mongodb');
 
-const mongoURI = process.env.mongoURI
+const mongoURI = "mongodb+srv://BookMyShow:BookMyShow@cluster0.hgdnylr.mongodb.net/?retryWrites=true&w=majority"
 
 let mongoose = require('mongoose');
 //  Importing Mongoose Schema from schema.js
